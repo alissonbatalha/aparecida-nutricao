fd25194c7a5a9bafef163c4746016bac6a9f7d3a
Alisson Batalha <82676146+alissonbatalha@users.noreply.github.com>
2023-01-23 14:26:13 -0300

    update

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -36,8 +36,7 @@ for (var i = 0; i < pacientes.length; i++) {
 }
 
 function calculaImc(peso, altura) {
-    var imc = 0;
-    imc = peso / (altura * altura);
+    var imc = imc = peso / (altura * altura);
 
     return imc.toFixed(2);
 }
